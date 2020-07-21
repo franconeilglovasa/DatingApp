25ee83235ac28a775f1f988d57a7131d599a49ca
Franco Neil Glovasa <franconeilglovasa@outlook.com>
2020-07-21 15:29:41 +0800

    End of Section 13

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -59,6 +59,7 @@ namespace DatingApp.API
                         ValidateAudience = false
                     };
                 });
+                services.AddScoped<LogUserActivity>(); // addscope because create new instances per request
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
